996b1e187411091520f513d8747a06c7a1da4ffd
alexaesqueda <alexaesqueda48@gmail.com>
2025-10-28 12:55:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
-from db import get_db, Base, engine
-from models import UserToken
-from auth import router as auth_router
+from src.db import get_db, Base, engine
+from src.models import UserToken
+from src.auth import router as auth_router
 
 # IMPORTANT: import the module itself so we can print its file path
-import spotify as spotify_module
-from spotify import (
+from src import spotify as spotify_module
+from src.spotify import (
     ensure_valid_token,
     get_me,
     recommend_tracks,
     create_playlist,
     add_tracks_to_playlist,
 )
-from vibes import VIBE_FEATURES
+from src.vibes import VIBE_FEATURES
 
 Base.metadata.create_all(bind=engine)
 
