67d3bae1334c9c359ee5036c89148573235ec8a0
shreyL07 <shreylingampalli@gmail.com>
2025-11-19 01:58:54 -0800

    Update streamlitFrontEnd.py

--- streamlitFrontEnd.py
+++ streamlitFrontEnd.py
@@ -321,7 +321,7 @@ def fetch_vibes():
 
 def header():
     # Create columns for title and connected status
-    left, right = st.columns([0.99, 0.01])
+    left, right = st.columns([1, 0])
     
     with left:
         # Center the title and tagline
