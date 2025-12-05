49d6b7c806e610f2b7f45e79e27f61676c8ddc05
Shivansh Gupta <shivanshg1947@gmail.com>
2025-12-05 01:45:46 -0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py (root of repo)
-
-from src.app.main import app  # import the Apple backend FastAPI instance
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
